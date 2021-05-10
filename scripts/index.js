c0b887edefec0a1615f16f0f81c7ee9ee8a43c7b
MaryamPayenda <maryampayanda12@gmail.com>
2021-05-10 11:56:59 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
-const theme = () => {
-  const mood = document.querySelector("#light-dark").checked;
-  body = document.querySelector("body").style;
-  console.log(mood);
-  if (mood) {
-    body.backgroundColor = "black";
-  } else {
-    body.backgroundColor = "white";
-  }
-};
+// const theme = () => {
+//   const mood = document.querySelector("#light-dark").checked;
+//   body = document.querySelector("body").style;
+//   console.log(mood);
+//   if (mood) {
+//     body.backgroundColor = "black";
+//   } else {
+//     body.backgroundColor = "white";
+//   }
+// };
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
